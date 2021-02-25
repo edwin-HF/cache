398cee8cb2658f6699ef5b847081905731e44671
edv <fanhang@minyea.com>
2021-02-25 16:46:54 +0800

    cache string 修改

--- src/Strategy/CacheString.php
+++ src/Strategy/CacheString.php
@@ -13,7 +13,6 @@ abstract class CacheString extends AbstractContext
 
     protected $expireAt;
     protected $expire;
-    protected $PREFIX = 'edv:cache:string:';
 
     public function exec(callable $callback)
     {
@@ -29,7 +28,7 @@ abstract class CacheString extends AbstractContext
         if (empty($key))
             return  '';
 
-        return unserialize(RedisUtil::client()->get($this->PREFIX . $key));
+        return unserialize(RedisUtil::client()->get($this->cacheKey() . $key));
 
     }
 
@@ -50,14 +49,14 @@ abstract class CacheString extends AbstractContext
 
         if (empty($key)){
 
-            if (empty($this->PREFIX))
+            if (empty($this->cacheKey()))
                 return $this;
 
             $iterator = null;
 
             do{
 
-                $keys = RedisUtil::client()->scan($iterator,$this->PREFIX . '*');
+                $keys = RedisUtil::client()->scan($iterator,$this->cacheKey() . '*');
 
                 foreach ($keys as $key){
                     RedisUtil::client()->del($key);
@@ -65,7 +64,7 @@ abstract class CacheString extends AbstractContext
 
             }while($iterator > 0);
         }else{
-            RedisUtil::client()->del($this->PREFIX . $key);
+            RedisUtil::client()->del($this->cacheKey() . $key);
         }
 
 
@@ -81,7 +80,7 @@ abstract class CacheString extends AbstractContext
     public function patchSelf(callable $callback , string $key = '')
     {
 
-        if (RedisUtil::client()->exists($this->PREFIX . $key))
+        if (RedisUtil::client()->exists($this->cacheKey() . $key))
             return $this->get($key);
 
         try {
@@ -92,14 +91,14 @@ abstract class CacheString extends AbstractContext
 
         $val = is_array($result) ? json_encode($result) : $result;
 
-        RedisUtil::client()->set($this->PREFIX . $key,serialize($val));
+        RedisUtil::client()->set($this->cacheKey() . $key,serialize($val));
 
         if ($this->expireAt){
-            RedisUtil::client()->expireAt($this->PREFIX . $key,$this->expireAt);
+            RedisUtil::client()->expireAt($this->cacheKey() . $key,$this->expireAt);
         }
 
         if ($this->expire){
-            RedisUtil::client()->expire($this->PREFIX . $key,$this->expire);
+            RedisUtil::client()->expire($this->cacheKey() . $key,$this->expire);
         }
 
         return $result;
